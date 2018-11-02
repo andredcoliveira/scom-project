8538aefdbe85ab347b817e68061815579679cc43
andredcoliveira <up201405639@fe.up.pt>
2018-11-02 17:38:46 +0000

    Setup complete up until installation of 'torrent-stream'

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Check if script is running as 'root'
+if (( $EUID != 0 )); then
+    echo "Please run as root. (e.g.: use 'sudo' command)"
+    exit
+fi
+
+RELEASE=$(lsb_release -a -s 2>/dev/null)
+
+# Install Node.js v11.x:
+if [[ "$RELEASE" =~ "Ubuntu" ]]; then
+#   Using Ubuntu
+  curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
+  sudo apt-get install -y nodejs
+elif [[ "$RELEASE" =~ "Debian" ]]; then
+#   Using Debian, ensure root
+  curl -sL https://deb.nodesource.com/setup_11.x | bash -
+  apt-get install -y nodejs
+else
+  echo -e "Need to install Nodejs manually. Check your distribution and follow the official instructions:\n\thttps://nodejs.org/en/download/package-manager/"
+fi
+
+# Verify that npm is up to date (needs 'sudo')
+npm install npm@latest -g
+
+# Create package.json automatically
+npm init -y
+
+# Install 'torrent-stream'
+npm install torrent-stream
