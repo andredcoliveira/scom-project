31ceb30f46431f725c8fb4b6ece6fe68e29abe4f
BaltasarAroso <ba_aroso@icloud.com>
2018-11-27 13:13:33 +0000

    Removing old and unused files

--- 03.read_js_files/old_read/testReader.js
+++
@@ -1,8 +0,0 @@
-var fileReader = require('./read_01.js')
-
-var txtFilename = 'magnets.txt';
-
-fileReader.parseFile(txtFilename, function(err, data){
-    if(err) return console.log(err);
-    console.log(data);
-});
--- 03.read_js_files/old_read/write.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs'),
-    lineno=0;
-
-var stream = fs.createWriteStream('test-read-write.txt', {flags:'a'});
-
-stream.on('open', function() {
-    console.log('Stream opened, will start writing in 2 secs');
-    setInterval(function() { stream.write('magnet:?xt=urn:btih:83f947976cb8adba5634cbd6794d40393220e018&dn=out00' + ++lineno + '.mp4 \n'); }, 2000);
-});
